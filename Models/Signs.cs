9d5be647e8b30650647c97fc35f8790cc66b7075
Brianca Knight <briancaknight@gmail.com>
2024-02-27 10:55:20 -0800

    add data validation to sign model

--- Models/Signs.cs
+++ Models/Signs.cs
@@ -3,9 +3,13 @@ namespace AstrologyApi.Models
   public class Sign
   {
     public int SignId { get; set; }
+    [Required]
     public string SignName { get; set; }
+    [Required]
     public string Expression { get; set; }
+    [Required]
     public string Description { get; set; }
+    [Required]
     public string Concentration { get; set; }
   }
 }
