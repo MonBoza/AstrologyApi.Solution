84e6f5f3d269802ff4906bf7d83fe176669d6a85
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-03-08 15:06:36 -0600

    Fix CORS policy and enable Swagger and HTTPS redirection

--- Program.cs
+++ Program.cs
@@ -1,50 +1,50 @@
 using AstrologyApi.Models;
 using Microsoft.EntityFrameworkCore;
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-options.AddPolicy(name: MyAllowSpecificOrigins,
-    policy => {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
 });
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AstrologyApiContext>(
-                  dbContextOptions => dbContextOptions
-                    .UseMySql(
-                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
-                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                    )
-                  )
-                );
+    dbContextOptions => dbContextOptions
+        .UseMySql(
+            builder.Configuration["ConnectionStrings:DefaultConnection"],
+            ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+        )
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else 
-{
-  app.UseHttpsRedirection();
-}
-
+// Enable CORS
 app.UseCors(MyAllowSpecificOrigins);
 
-app.UseAuthorization();
+// Enable Swagger for API documentation
+app.UseSwagger();
+app.UseSwaggerUI();
 
+// Enable HTTPS redirection in production
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
